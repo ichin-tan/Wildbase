a85ac0c6c3f73e9678a0fb83e01dbc05e9f666da
ichin-tan <chintanpanchal396@gmail.com>
2025-10-30 15:37:13 -0400

    Initial Commit

---
+++ Wildbase/WildbaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildbaseApp.swift
+//  Wildbase
+//
+//  Created by CP on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WildbaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
