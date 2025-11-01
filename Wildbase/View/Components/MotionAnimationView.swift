5d7b75e7a5894cf777f3794e875b6c91254fd99b
ichin-tan <chintanpanchal396@gmail.com>
2025-10-31 21:57:32 -0400

    created amazing motion animation

---
+++ Wildbase/View/Components/MotionAnimationView.swift
@@ -0,0 +1,61 @@
+//
+//  MotionAnimationView.swift
+//  Wildbase
+//
+//  Created by CP on 31/10/25.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    
+    @State var isAnimating = false
+    @State private var numberOfCircles = Int.random(in: 10...15)
+    
+    func getRandomCoordinate(max: CGFloat) -> CGFloat {
+        return CGFloat.random(in: 0...max)
+    }
+    
+    func getRandomSize() -> CGFloat {
+        return CGFloat.random(in: 50...70)
+    }
+    
+    func getRandomScale() -> CGFloat {
+        return CGFloat.random(in: 0.1...2.0)
+    }
+    
+    func getRandomSpeed() -> CGFloat {
+        return CGFloat.random(in: 0.3...1.0)
+    }
+    
+    func getRandomDelay() -> CGFloat {
+        return CGFloat.random(in: 0...1)
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            
+            ZStack {
+                ForEach(0...numberOfCircles, id: \.self) { _ in
+                    Circle()
+                        .foregroundColor(.accent)
+                        .opacity(0.15)
+                        .frame(width: self.getRandomSize(), height: self.getRandomSize(), alignment: .center)
+                        .scaleEffect(getRandomScale())
+                        .position(x: self.getRandomCoordinate(max: geometry.size.width), y: self.getRandomCoordinate(max: geometry.size.height)
+                        )
+                        .animation(.interpolatingSpring(stiffness: 0.5, damping: 0.5).repeatForever().speed(getRandomSpeed()).delay(getRandomDelay()), value: isAnimating)
+                        .onAppear() {
+                            isAnimating = true
+                        }
+                }
+            }
+            .drawingGroup()
+        }
+        
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
